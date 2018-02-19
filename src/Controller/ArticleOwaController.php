bdf122c04eb16831766a6da5042ecc8266c236fc
riko781 <henrickcarneva@gmail.com>
2018-02-19 23:39:35 +0100

    template

--- src/Controller/ArticleOwaController.php
+++ src/Controller/ArticleOwaController.php
@@ -6,6 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ArticleOwaController extends Controller
 {
@@ -14,10 +17,47 @@ class ArticleOwaController extends Controller
      * @Route("/owa", name="owa")
      */
     public function afficheArticleOwa(request $request){
-      $article=new Article();
-      $article = $this->getDoctrine()
-      ->getRepository(Article::class)
-      ->afficheListeArticle();
+        if(!empty($_GET['form']['designation']) && !empty($_GET['form']['categorie'])){
+            $designation=$_GET['form']['designation'];
+            $categorie=$_GET['form']['categorie'];
+            $article=new Article();
+            $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->chercheUnArticle($designation,$categorie);
+         
+           }elseif(!empty($_GET['form']['categorie']) && empty($_GET['form']['designation'])){
+          
+            $categorie=$_GET['form']['categorie'];
+            $article=new Article();
+            $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->chercheUneCategorie($categorie);
+          
+           }elseif(!empty($_GET['form']['designation']) && empty($_GET['form']['categorie'])){
+             
+              $designation=$_GET['form']['designation'];
+              $article=new Article();
+              $article = $this->getDoctrine()
+              ->getRepository(Article::class)
+              ->chercheUneDesignation($designation);
+            
+           }else{
+            $article=new Article();
+            $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->afficheListeArticle();    
+           };
+          
+            $form = $this->createFormBuilder($article)
+            ->setMethod('GET')
+            ->add('designation', TextType::class,array('label' => false,'required' => false, 'attr' => array('class' => 'srchTxt','placeholder'=>'Recherche')))
+            ->add('categorie', ChoiceType::class, array('label' => false,'required' => false,'attr' => array('class' => 'srchTxt'),'choices'=>array(
+              'Tout' => '',
+              'Homme' => 'homme',
+              'Femme' => 'femme'
+            )))
+            ->add('save', SubmitType::class, array('label' => 'Recherche','attr' => array('id' =>'submitButton','class' => 'btn btn-primary')))
+            ->getForm();
 
       $paginator  = $this->get('knp_paginator');
       $pagination = $paginator->paginate(
@@ -26,7 +66,7 @@ class ArticleOwaController extends Controller
              $request->query->getInt('limit', 5)/*limit per page*/
          );
          // parameters to template
-         return $this->render('owa/index.html.twig', array('pagination' => $pagination));
+         return $this->render('owa/index.html.twig', array('pagination' => $pagination,'form' =>$form->createView()));
     }
 
 }
