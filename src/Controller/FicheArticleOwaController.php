275f6faf9e7dde05cbdc2c9d6084faf58cb9c67a
riko781 <henrickcarneva@gmail.com>
2018-02-21 12:35:39 +0100

    form FicheaArticle

--- src/Controller/FicheArticleOwaController.php
+++ src/Controller/FicheArticleOwaController.php
@@ -14,11 +14,22 @@ class FicheArticleOwaController extends Controller
    * @Route("/owa/ficheArticle{id}", name="ficheArticleOwa")
     */
     public function index($id){
+		 $form = $this->createFormBuilder($article)
+            ->setMethod('GET')
+            ->add('designation', TextType::class,array('label' => false,'required' => false, 'attr' => array('class' => 'srchTxt','placeholder'=>'Recherche')))
+            ->add('categorie', ChoiceType::class, array('label' => false,'required' => false,'attr' => array('class' => 'srchTxt'),'choices'=>array(
+              'Tout' => '',
+              'Fruit' => 'fruit',
+              'Legume' => 'legume',
+            )))
+            ->add('save', SubmitType::class, array('label' => 'Recherche','attr' => array('id' =>'submitButton','class' => 'btn btn-primary')))
+            ->getForm();
+		
       $article=new Article();
       $article = $this->getDoctrine()
       ->getRepository(Article::class)
       ->trouveUnArticle($id);
-      return $this->render('owa/ficheArticle.html.twig', array('produit' =>$article,'suggestion'=>'bordel'));
+      return $this->render('owa/ficheArticle.html.twig', array('produit' =>$article,'suggestion'=>'bordel','form' =>$form->createView()));
     }
 
 }
