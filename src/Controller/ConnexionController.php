7dda4a551988a3c9f181ba0ba7a40c721032175d
riko781 <henrickcarneva@gmail.com>
2018-02-21 22:22:32 +0100

    fixed bug

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -27,8 +27,8 @@ class ConnexionController extends Controller
       ->afficheListeClient();
       //récup tous les clients
       $form = $this->createFormBuilder($client)
-      ->add('inputEmail', TextType::class,array('label'=> false,'attr'=>array('placeholder'=>'Email')))
-      ->add('inputPassword', TextType::class,array('label'=> false,'attr'=>array('placeholder'=>'Email')))
+      ->add('Email', TextType::class,array('label'=> false,'attr'=>array('placeholder'=>'Email')))
+      ->add('Password', TextType::class,array('label'=> false,'attr'=>array('placeholder'=>'Email')))
       ->add('save', SubmitType::class, array('label' => 'Se Connecter','attr'=>array('class'=>'btn btn-success')))
       ->getForm();
       $form->handleRequest($request);
