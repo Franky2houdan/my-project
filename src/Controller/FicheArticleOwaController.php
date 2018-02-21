81366f7241da707aaf893ef726449dcbe21efae3
riko781 <henrickcarneva@gmail.com>
2018-02-21 13:34:45 +0100

    add use

--- src/Controller/FicheArticleOwaController.php
+++ src/Controller/FicheArticleOwaController.php
@@ -7,6 +7,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class FicheArticleOwaController extends Controller
 {
