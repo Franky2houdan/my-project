c5707d04fa3420140e38301b9b840f23d7cb38cc
riko781 <henrickcarneva@gmail.com>
2018-02-21 22:20:26 +0100

    sixed bug

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -27,9 +27,9 @@ class ConnexionController extends Controller
       ->afficheListeClient();
       //récup tous les clients
       $form = $this->createFormBuilder($client)
-      ->add('inputEmail', TextType::class,array('label',false,'attr'=>array('placeholder'=>'Email')))
-      ->add('inputPassword', TextType::class,array('label',false,'attr'=>array('placeholder'=>'Email')))
-      ->add('save', SubmitType::class, array('label' => 'Se Connecter','attr'=>array('class','btn btn-success')))
+      ->add('inputEmail', TextType::class,array('label'=> false,'attr'=>array('placeholder'=>'Email')))
+      ->add('inputPassword', TextType::class,array('label'=> false,'attr'=>array('placeholder'=>'Email')))
+      ->add('save', SubmitType::class, array('label' => 'Se Connecter','attr'=>array('class'=>'btn btn-success')))
       ->getForm();
       $form->handleRequest($request);
       /*comparer la saisie avec la base*/
