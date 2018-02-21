ebd2698095474266f63fa8414d93303494a98be4
riko781 <henrickcarneva@gmail.com>
2018-02-21 16:47:14 +0100

    pagination 8

---
+++ src/Controller/CompteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Article;
+use App\Entity\Fournisseur;
+use App\Entity\Achat;
+
+class AchatController extends Controller
+{
+    /**
+     * @Route("/siteCom/compte", name="compte")
+     */
+    public function index()
+    {
+            return new response('comte');
+    }
+
+}
